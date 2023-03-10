ce1b0ad1f2c95844ab9a2ed8374c8139e4fd0e3a
rudradcruze <francisrudra@gmail.com>
2023-03-10 19:00:17 +0600

    initial upload

---
+++ src/ac/dia/shortcourse/Account.java
@@ -0,0 +1,68 @@
+package ac.dia.shortcourse;
+
+public class Account {
+	
+	private int accountNo;
+	private String accountTitle;
+	private double accountBalance;
+	
+	
+	public int getAccountNo() {
+		return accountNo;
+	}
+	
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	
+	public String getAccountTitle() {
+		return accountTitle;
+	}
+	
+	public void setAccountTitle(String accountTitle) {
+		this.accountTitle = accountTitle;
+	}
+	
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+	
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	
+	public void deposit(double amount) {
+		
+		accountBalance += amount;
+		
+		System.out.println("==== !!! ==== Deposit Successful ==== !!! ====");
+		System.out.println("Account No: " + getAccountNo());
+		System.out.println("Deposit amount: " + amount);
+		System.out.println("Your new balance is: " + getAccountBalance() + "\n");
+	}
+	
+	public void withdraw(double amount) {
+		
+		if(amount > getAccountBalance()) {
+			System.out.println("==== !!! ==== Withdraw Unsuccessful ==== !!! ====");
+			System.out.println("The amount " + amount + " you want to withdraw  is gratter then the actual balance.");
+			return;
+		}
+
+		accountBalance -= amount;
+		
+		System.out.println("==== !!! ==== Withdraw Successful ==== !!! ====");
+		System.out.println("Account No: " + getAccountNo());
+		System.out.println("Withdraw amount: " + amount);
+		System.out.println("Your new balance is: " + getAccountBalance() + "\n");
+	}
+	
+	public void showDetails() {
+		System.out.println("------ Account Information ------");
+		System.out.println("---------------------------------");
+		System.out.println("- Account No: " + getAccountNo());
+		System.out.println("- Account Title: " + getAccountTitle());
+		System.out.println("- Account Balance: " + getAccountBalance());
+		System.out.println("---------------------------------\n");
+	}
+}
---
+++ src/ac/dia/shortcourse/Main.java
@@ -0,0 +1,27 @@
+package ac.dia.shortcourse;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Account account = new Account();
+		
+		account.showDetails();	// Show default cause the value is not set yet.
+		
+		account.setAccountNo(1001);
+		account.setAccountTitle("Rudra");
+		account.setAccountBalance(15000);
+		
+		// Details show after set
+		account.showDetails();
+		
+		// Deposit and show
+		account.deposit(10000);
+//		account.showDetails();
+		
+		// Withdraw and show
+		account.withdraw(2000);
+		account.withdraw(30000);
+//		account.showDetails();
+	}
+}
